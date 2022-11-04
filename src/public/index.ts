3253b83951e426cc505e8ae000a8070db3449718
张幸铖 <studentzxc@gmail.com>
2022-11-04 06:11:46 +0000

    feat: added skeleton for react app

--- src/public/index.ts
+++ src/public/index.ts
@@ -1,1 +1,9 @@
+import { createElement, StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./features/home/App";
 import "./styles.less";
+
+const root = createRoot(document.getElementById("app"));
+const app = createElement(App);
+const strictMode = createElement(StrictMode, undefined, app);
+root.render(strictMode);
